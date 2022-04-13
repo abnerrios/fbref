c4cfcc3781954af65662ae78b4b97a4529ec3bc2
abnerrios <abner0rios@gmail.com>
2022-04-13 11:00:10 -0300

    Criação do método ScheduledMatchd.describe()

--- fbref/element.py
+++ fbref/element.py
@@ -1,7 +1,8 @@
-
 from urllib.parse import urljoin
 import requests
 import re
+import json
+import csv
 from bs4 import BeautifulSoup
 from datetime import datetime
 from handlers import PreviousMatchHandlers
@@ -12,25 +13,23 @@ class ScheduledMatches:
 
         See following example:
 
-        .. code-block:: python
+            matches = Matches()
 
-        matches = Matches()
-        matches.day_matches()
-
-        :params date: 'YYYY-MM-DD'
+            matches.day_matches('YYYY-MM-DD')
 
     """
-    def __init__(self, data: str) -> None:
-        self._data = data
-
-    def _handle_data(self, data) -> str:
+    def _handle_date(self, date) -> str:
         
-        if not data:
+        if not date:
             return datetime.now().strftime('YYYY-MM-DD')
 
-    def day_matches(self) -> list:
-        """ """
-        rsp = requests.request('GET', f'https://fbref.com/en/matches/{self._data}')
+    def day_matches(self, date) -> list:
+        """Return matches from specified date.
+
+        :params date: 'YYYY-MM-DD'
+        """
+        date = self._handle_date(date)
+        rsp = requests.request('GET', f'https://fbref.com/en/matches/{date}')
         content = rsp.content
         day_matches = []
 
@@ -58,9 +57,10 @@ class ScheduledMatches:
 
                     # parse name when country comes first
                     match.away = re.sub('^[a-z]+\s', '', match.away)
+                    match.home = re.sub('\s+[a-z]{2}$', '', match.home)
 
                     home_a = match_dict.get('squad_a').find('a')
-                    away_a = match_dict.get('squad_a').find('a')
+                    away_a = match_dict.get('squad_b').find('a')
 
                     match._home_ref = home_a.attrs.get('href')
                     match._away_ref = away_a.attrs.get('href')
@@ -71,10 +71,6 @@ class ScheduledMatches:
 
         return day_matches
 
-
-class MatchReport:
-    def __init__(self) -> None:
-        pass
 
 class PreviousMatch:
     def __init__(self) -> None:
@@ -88,16 +84,32 @@ class PreviousMatch:
         self.formation = str
         self.possession = str
         self.captain = str
-        self.match_report = MatchReport()
+        self.corners = str
+        self.shots = int
+        self.shots_on_target = int
+        self.offsides = int
+        self.fouls = int
+        self.match_summary = dict
 
 
 class Squad(PreviousMatchHandlers):
-    def __init__(self, name) -> None:
+    def __init__(self, name, competition, venue) -> None:
         super().__init__()
-        self.name = name
+        self.name = re.sub('\s+[a-z]{2}$', '', name)
+        self._competition = competition
+        self._venue = venue
         self.history = []
 
-    def match_summary(self, href, previous_matches) -> None:
+    def match_summary(self, href, previous_matches, competitions, venue) -> None:
+        VALID_COMPETITIONS = ['all', 'same']
+        VALID_VENUES = ['home', 'away', 'any']
+
+        if competitions.lower() not in VALID_COMPETITIONS:
+            raise ValueError("competitions: status must be one of %r." % VALID_COMPETITIONS)
+
+        if venue.lower() not in VALID_VENUES:
+            raise ValueError("venue: status must be one of %r." % VALID_VENUES)
+
         squad_url = urljoin('https://fbref.com', href)
         previous_matches = self._handle_previous_matches(squad_url, previous_matches)
 
@@ -109,7 +121,7 @@ class Squad(PreviousMatchHandlers):
             previous_match.venue = match.get('venue').text
             previous_match.opponent = match.get('opponent').text 
 
-            # parse name when country comes first
+            # parse name when country comes first or at the end
             previous_match.opponent = re.sub('^[a-z]+\s', '', previous_match.opponent)
 
             previous_match.goals_for = int(match.get('goals_for').text)
@@ -117,11 +129,23 @@ class Squad(PreviousMatchHandlers):
             previous_match.formation = match.get('formation').text
             previous_match.possession = float(match.get('possession').text) if match.get('possession').text else None 
             previous_match.captain = match.get('captain').text
-            previous_match.match_report = self._handle_match_report(match.get('match_report').find('a').attrs.get('href'))
+
+            # collect match details 
+            match_report = self._handle_match_report(
+                match_url = match.get('match_report').find('a').attrs.get('href'), 
+                venue = previous_match.venue
+            )
+            previous_match.corners = match_report['corners']
+            previous_match.shots = match_report['shots']
+            previous_match.shots_on_target = match_report['shots_on_target']
+            previous_match.offsides = match_report['offsides']
+            previous_match.fouls = match_report['fouls']
+            previous_match.match_summary = match_report['summary']
 
             self.history.append(previous_match)
 
     def results(self) -> dict:
+        """ """
         results = {'W':0, 'L': 0, 'D': 0}
 
         for match in self.history:
@@ -131,16 +155,150 @@ class Squad(PreviousMatchHandlers):
             if match.result=='L':
                 results['L'] +=1
             
-            if match.result=='W':
+            if match.result=='D':
                 results['D'] +=1
         
         return results 
 
-    def to_dict(self) -> dict:
-        pass
+    def corners(self) -> dict:
+        """ """
+        total = 0
+        avg = None
 
-    def to_json(self) -> dict:
-        pass
+        for match in self.history:
+            if match.corners:
+                total+=match.corners
+        
+        avg = round(total/len(self.history), 2)
+
+        return {'total': total, 'avg': avg}
+
+    def fouls(self) -> dict:
+        """ """
+        total = 0
+        avg = None
+
+        for match in self.history:
+            if match.fouls:
+                total+=match.fouls
+        
+        avg = round(total/len(self.history), 2)
+
+        return {'total': total, 'avg': avg}
+
+    def offsides(self) -> dict:
+        """ """
+        total = 0
+        avg = None
+
+        for match in self.history:
+            if match.offsides:
+                total+=match.offsides
+        
+        avg = round(total/len(self.history), 2)
+
+        return {'total': total, 'avg': avg}
+        
+    def shots(self) -> dict:
+        """ """
+        total = 0
+        avg = None
+
+        for match in self.history:
+            if match.shots:
+                total+=match.shots
+        
+        avg = round(total/len(self.history), 2)
+
+        return {'total': total, 'avg': avg}
+        
+    def goals_for(self) -> dict:
+        """ """
+        total = 0
+        avg = None
+
+        for match in self.history:
+            if match.goals_for:
+                total+=match.goals_for
+        
+        avg = round(total/len(self.history), 2)
+
+        return {'total': total, 'avg': avg}
+        
+    def goals_against(self) -> dict:
+        """ """
+        total = 0
+        avg = None
+
+        for match in self.history:
+            if match.goals_against:
+                total+=match.goals_against
+        
+        avg = round(total/len(self.history), 2)
+
+        return {'total': total, 'avg': avg}
+
+    def to_dict(self) -> list:
+        
+        team_history = []
+
+        for match in self.history:
+            team_history.append({
+                'time': match.time,
+                'competition': match.competition,
+                'result': match.result,
+                'venue': match.venue,
+                'opponent': match.opponent,
+                'goals_for': match.goals_for,
+                'goals_against': match.goals_against,
+                'formation': match.formation,
+                'possession': match.possession,
+                'captain': match.captain,
+                'corners': match.corners,
+                'shots': match.shots,
+                'shots_on_target': match.shots_on_target,
+                'offsides': match.offsides,
+                'fouls': match.fouls,
+                'match_summary': match.match_summary
+            })
+        
+        return team_history
+
+    def to_json(self) -> str:
+        
+        team_history = []
+
+        for match in self.history:
+            team_history.append({
+                'time': match.time,
+                'competition': match.competition,
+                'result': match.result,
+                'venue': match.venue,
+                'opponent': match.opponent,
+                'goals_for': match.goals_for,
+                'goals_against': match.goals_against,
+                'formation': match.formation,
+                'possession': match.possession,
+                'captain': match.captain,
+                'corners': match.corners,
+                'shots': match.shots,
+                'shots_on_target': match.shots_on_target,
+                'offsides': match.offsides,
+                'fouls': match.fouls,
+                'match_summary': match.match_summary
+            })
+        
+        return json.dumps(team_history)
+
+    def to_csv(self, path: str) -> None:
+
+        data = self.to_dict()
+        with open(path, 'w', newline='') as csvfile:
+            fieldnames = data[0].keys()
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            writer.writerows(data)
 
 
 class ScheduledMatch:
@@ -155,34 +313,60 @@ class ScheduledMatch:
         self._away_ref = str
 
     def display(self) -> str:
-        return f"""=====* {self.competition} *=====
+        return f"""=====************=====
+        🏆 {self.competition} 
         ⚽️ {self.date}
         {self.home} X {self.away}
         🏟  {self.venue}
         """
 
-    def home_stats(self, previous_matches: int) -> Squad:
+    def home_stats(self, previous_matches: int, competitions: str, venue= str) -> Squad:
         """Return statistics from Home team last N `~previous_matches` games.
 
         :params previous_matches: number of matches to considerate on summary.
         """
-        squad = Squad(name=self.home)
-        squad.match_summary(href=self._home_ref, previous_matches=previous_matches)
+        squad = Squad(name=self.home, competition=self.competition, venue='Home')
+        squad.match_summary(href=self._home_ref, previous_matches=previous_matches, competitions=competitions, venue=venue)
 
         return squad
     
-    def away_stats(self, previous_matches: int) -> Squad:
+    def away_stats(self, previous_matches: int, competitions: str, venue= str) -> Squad:
         """Return statistics from Away team last N `~previous_matches` games.
 
         :params previous_matches: number of matches to considerate on summary.
         """
-        squad = Squad(name=self.away)
-        squad.match_summary(href=self._away_ref, previous_matches=previous_matches)
+        squad = Squad(name=self.away, competition=self.competition, venue='Away')
+        squad.match_summary(href=self._away_ref, previous_matches=previous_matches, competitions=competitions, venue=venue)
 
         return squad
+    
+    def describe(self) -> str:
+        home = self.home_stats(previous_matches=5, competitions='all', venue='any')
+        away = self.away_stats(previous_matches=5, competitions='all', venue='any')
+
+        return f"""
+            =====*******************************=====
+            🏆 {self.competition} 
+            ⚽️ {self.date}
+            🏟  {self.venue}
+            |---------------------------------------|
+            | {self.home}
+            | goals. {home.goals_for()['avg']}
+            | corners. {home.corners()['avg']}
+            | fouls. {home.fouls()['avg']}
+            | shots. {home.shots()['avg']}
+            | offsides. {home.offsides()['avg']}
+            |---------------------------------------|
+            | {self.away}
+            | goals. {away.goals_for()['avg']}
+            | corners. {away.corners()['avg']}
+            | fouls. {away.fouls()['avg']}
+            | shots. {away.shots()['avg']}
+            | offsides. {away.offsides()['avg']}
+        
+        """
 
 
-
-matches = ScheduledMatches(data='2022-04-12')
-for match in matches.day_matches():
-    match.home_stats(previous_matches=5)
+matches = ScheduledMatches()
+for match in matches.day_matches(date='2022-04-13'):
+    print(match.describe())
--- fbref/handlers.py
+++ fbref/handlers.py
@@ -31,56 +31,93 @@ class PreviousMatchHandlers(object):
         matches.reverse()
 
         for i in range(previous_matches):
-            match = matches[i]
-            last_matches.append(match)
+            if i<len(matches):
+                match = matches[i] 
+                last_matches.append(match)
 
         return last_matches
 
-    def _handle_match_report(self, match_url) -> dict:
+    def _handle_match_report(self, match_url: str, venue: str) -> dict:
         match_report = {
-            'possesion': 0, 
-            'shots': 0,
-            'shots_on_target': 0,
-            'corners': 0,
-            'offsides': 0,
-            'fouls': 0,
-            'summary': {}
+            'shots': None,
+            'shots_on_target': None,
+            'corners': None,
+            'offsides': None,
+            'fouls': None,
+            'summary': []
         }
 
         url = urljoin('https://fbref.com/', match_url)
         rsp = requests.request('GET', url)
-        cleanr = re.compile('<.*?>|/|\n|\t|\xa0|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{a,6});')
+        cleanr = re.compile('<.*?>|/|\n|\t|\xa0|—|\d+%|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{a,6});')
+        venue_event_class = 'a' if venue=='Home' else 'b'
 
         if rsp.status_code<400:
             content = rsp.content
             soup = BeautifulSoup(content, 'html.parser')
-            scorebox = soup.find('div', attrs={'class': 'scorebox'})
+            team_stats = soup.find('div', attrs={'id': 'team_stats'})
+
+            # Shots on Target
+            shots_on_target = team_stats.find('tr', text='Shots on Target')
+
+            if shots_on_target:
+                i = 0 if venue=='Home' else 1
+                shot_values = shots_on_target.find_next('tr')
+                team_shot = shot_values.find_all('td')[i]
+
+                shots_text = team_shot.find('div').find('div').text
+                # clean simbols and accuracy of text
+                shots_text = re.sub(cleanr,'',shots_text)
+                match_report['shots'] = int(shots_text.split(' of ')[1])
+                match_report['shots_on_target'] = int(shots_text.split(' of ')[0])
+
+            team_stats_extra = soup.find('div', attrs={'id': 'team_stats_extra'})
+
+            if team_stats_extra:
+                has_fouls = team_stats_extra.find('div', text='Fouls')
+                has_corners = team_stats_extra.find('div', text='Corners')
+                has_offsides = team_stats_extra.find('div', text='Offsides')
+                fouls = None
+                corners = None
+                offsides = None
+
+                if venue=='Home':
+                    fouls = int(has_fouls.find_previous('div').text) if has_fouls else None
+                    corners = int(has_corners.find_previous('div').text) if has_corners else None
+                    offsides = int(has_offsides.find_previous('div').text) if has_offsides else None
+
+                if venue=='Away':
+                    fouls = int(has_fouls.find_next('div').text) if has_fouls else None
+                    corners = int(has_corners.find_next('div').text) if has_corners else None
+                    offsides = int(has_offsides.find_next('div').text) if has_offsides else None
+                
+                match_report['fouls'] = fouls
+                match_report['corners'] = corners
+                match_report['offsides'] = offsides
+
+            
             events_wrap = soup.find('div', attrs={'id': 'events_wrap'})
-            events = events_wrap.find_all('div', attrs={'class': 'event'})
+            if events_wrap:
+                events = events_wrap.find_all('div', attrs={'class': f'event {venue_event_class}'})
 
-            performers = scorebox.find_all('a', attrs={'itemprop': 'name'})
-            home = performers[0].text
-            away = performers[1].text
+                for event in events:
+                    cleanr = re.compile('<.*?>|/|\n|\t|\xa0|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{a,6});')
+                    minute_div = event.find_all('div')[0]
+                    eventtype_div = event.find_all('div')[1]
+                    # clean simbols of text
+                    cleaned_minute = re.sub(cleanr, '', minute_div.text)
+                    cleaned_event = re.sub(cleanr, '', eventtype_div.text)
+                    # return only relevant parts of events
+                    minute = re.sub(r'\’.+','', cleaned_minute)
+                    event = cleaned_event.split('—')[1].split(' ')[0]
+                    player_event = cleaned_event.split('—')[0].split(':')
+                    player_event = player_event[1] if len(player_event)>1 else player_event[0]
 
-            match_report['summary'].update({home: [], away: []})
-
-            for event in events:
-                team = home if 'a' in event.attrs.get('class') else away
-                minute_div = event.find_all('div')[0]
-                eventtype_div = event.find_all('div')[1]
-                # clean simbols of text
-                cleaned_minute = re.sub(cleanr, '', minute_div.text)
-                cleaned_event = re.sub(cleanr, '', eventtype_div.text)
-                # return only relevant parts of events
-                minute = re.sub(r'\’.+','', cleaned_minute)
-                player_event = cleaned_event.split('—')[0]
-                event = cleaned_event.split('—')[1]
-
-                if event!='Substitute':
-                    match_report['summary'][team].append({
-                        'minute': minute,
-                        'eventtype': event,
-                        'player': player_event
-                    })
+                    if event!='Substitute':
+                        match_report['summary'].append({
+                            'minute': minute,
+                            'eventtype': event,
+                            'player': player_event
+                        })
 
         return match_report
